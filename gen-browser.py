03abeb538c8f791f915844035e00bd63dbc74f71
Luiz Eduardo Bastos <119376771+luizEduardoBastos@users.noreply.github.com>
2026-05-31 19:41:56 -0300

    Delete gen-browser.py

--- gen-browser.py
+++
@@ -1,8 +0,0 @@
-import ytmusicapi
-
-with open("headers.txt", "r", encoding="utf-8") as f:
-    headers_raw = f.read()
-
-ytmusicapi.setup(filepath="browser.json", headers_raw=headers_raw)
-
-print("Browser.json created successfully!")
